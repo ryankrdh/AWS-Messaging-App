692453f1c3834aeff30da72964515c9b02fa0dd1
ryankrdh <ryankrdh@gmail.com>
2023-07-13 12:19:13 -0700

    static data server test

--- Lambda/Chat-Proxy.js
+++ Lambda/Chat-Proxy.js
@@ -3,6 +3,7 @@
 var AWS = require('aws-sdk');
 
 var S3 = new AWS.S3();
+var dynamo = new AWS.DynamoDB();
 
 var bucket = '<your bucket name>';
 
@@ -30,13 +31,69 @@ exports.handler = function (event, context, callback) {
         });
     } else if (path.startsWith('conversations/')) {
         var id = path.substring('conversations/'.length);
-        S3.getObject({
-            Bucket: bucket,
-            Key: 'data/conversations/' + id + '.json'
+        dynamo.query({
+            TableName: 'Chat-Messages',
+            ProjectionExpression: '#T, Sender, Message',
+            ExpressionAttributeNames: {'#T': 'Timestamp'},
+            KeyConditionExpression: 'ConversationId = :id',
+            ExpressionAttributeValues: {':id': {S: id}}
         }, function (err, data) {
-            done(err, err ? null : JSON.parse(data.Body.toString()));
+            loadMessages(err, data, id, [], done);
         });
     } else {
         done('No cases hit');
     }
 };
+
+function loadMessages(err, data, id, messages, callback) {
+    if (err === null) {
+        data.Items.forEach(function (message) {
+            messages.push({
+                sender: message.Sender.S,
+                time: Number(message.Timestamp.N),
+                message: message.Message.S
+            });
+        });
+        if(data.LastEvaluatedKey) {
+            dynamo.query({
+                TableName: 'Chat-Messages',
+                ProjectionExpression: '#T, Sender, Message',
+                KeyConditionExpression: 'ConversationId = :id',
+                ExpressionAttributeNames: {'#T': 'Timestamp'},
+                ExpressionAttributeValues: {':id': {S: id}},
+                ExclusiveStartKey: data.LastEvaluatedKey
+            }, function (err, data) {
+                loadMessages(err, data, id, messages, callback);
+            });
+        } else {
+            loadConversationDetail(id, messages, callback);
+        }
+    } else {
+        callback(err);
+    }
+}
+
+function loadConversationDetail(id, messages, callback) {
+    dynamo.query({
+        TableName: 'Chat-Conversations',
+        Select: 'ALL_ATTRIBUTES',
+        KeyConditionExpression: 'ConversationId = :id',
+        ExpressionAttributeValues: {':id': {S: id}}
+    }, function (err, data) {
+        if (err === null) {
+            var participants = [];
+            data.Items.forEach(function (item) {
+                participants.push(item.Username.S);
+            });
+
+            callback(null, {
+                id: id,
+                participants: participants,
+                last: messages.length > 0 ? messages[messages.length-1].time : undefined,
+                messages: messages
+            });
+        } else {
+            callback(err);
+        }
+    });
+}
--- Site/js/site.js
+++ Site/js/site.js
@@ -1,57 +1,71 @@
 var ChatApp = window.ChatApp || {};
 
 (function scopeWrapper($) {
+  var currentUsername = 'Student';
+  var apiEndpoint = ChatApp.apiEndpoint;
 
-    var currentUsername = 'Student';
-    var apiEndpoint = ChatApp.apiEndpoint;
-
-    ChatApp.populateChats = function () {
-        $.get(apiEndpoint + '/conversations').done(function (data) {
-            data.forEach(function (convo) {
-                var otherUsers = [];
-                convo.participants.forEach(function (user) {
-                    if (user !== currentUsername) {
-                        otherUsers.push(user);
-                    }
-                });
-
-                $('TBODY').append('<tr><td><a href="chat.html#' + convo.id + '">' + otherUsers.join(', ') + '</a></td></tr>');
-            });
-            $('TBODY').append('<tr><td></td></tr>');
+  ChatApp.populateChats = function () {
+    $.get(apiEndpoint + '/conversations').done(function (data) {
+      data.forEach(function (convo) {
+        var otherUsers = [];
+        convo.participants.forEach(function (user) {
+          if (user !== currentUsername) {
+            otherUsers.push(user);
+          }
         });
-    };
 
-    ChatApp.loadChat = function () {
-        $.get(apiEndpoint + '/conversations/' + location.hash.substring(1)).done(function (result) {
-            result.messages.forEach(function (message) {
-                var panel = $('<div class="panel">');
-                if (message.sender === currentUsername) {
-                    panel.addClass('panel-default');
-                } else {
-                    panel.addClass('panel-info');
-                    panel.append('<div class="panel-heading">' + message.sender + '</div>');
-                }
-                var body = $('<div class="panel-body">').text(message.message);
-                panel.append(body);
-                panel.append('<div class="panel-footer messageTime" data-time="' + message.time + '">' + moment(message.time).fromNow() + '</div>');
+        $('TBODY').append(
+          '<tr><td><a href="chat.html#' +
+            convo.id +
+            '">' +
+            otherUsers.join(', ') +
+            '</a></td></tr>'
+        );
+      });
+      $('TBODY').append('<tr><td></td></tr>');
+    });
+  };
 
-                var row = $('<div class="row">');
-                var buffer = $('<div class="col-xs-4">');
-                var holder = $('<div class="col-xs-8">');
-                holder.append(panel);
+  ChatApp.loadChat = function () {
+    $.get(apiEndpoint + '/conversations/' + location.hash.substring(1)).done(
+      function (result) {
+        result.messages.forEach(function (message) {
+          var panel = $('<div class="panel">');
+          if (message.sender === currentUsername) {
+            panel.addClass('panel-default');
+          } else {
+            panel.addClass('panel-info');
+            panel.append(
+              '<div class="panel-heading">' + message.sender + '</div>'
+            );
+          }
+          var body = $('<div class="panel-body">').text(message.message);
+          panel.append(body);
+          panel.append(
+            '<div class="panel-footer messageTime" data-time="' +
+              message.time +
+              '">' +
+              moment(message.time).fromNow() +
+              '</div>'
+          );
 
-                if (message.sender === currentUsername) {
-                    row.append(buffer);
-                    row.append(holder);
-                } else {
-                    row.append(holder);
-                    row.append(buffer);
-                }
+          var row = $('<div class="row">');
+          var buffer = $('<div class="col-xs-4">');
+          var holder = $('<div class="col-xs-8">');
+          holder.append(panel);
 
-                $('#chat').append(row);
-            });
-            window.scrollTo(0, document.body.scrollHeight);
+          if (message.sender === currentUsername) {
+            row.append(buffer);
+            row.append(holder);
+          } else {
+            row.append(holder);
+            row.append(buffer);
+          }
+
+          $('#chat').append(row);
         });
-    };
-
-}(jQuery));
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+    );
+  };
+})(jQuery);
