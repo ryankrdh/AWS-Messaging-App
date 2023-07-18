8d62f8b3f7e67b6eeb23d1f8a6cc834d92b58619
ryankrdh <ryankrdh@gmail.com>
2023-07-18 09:33:32 -0700

    signing users in

--- Site/js/site.js
+++ Site/js/site.js
@@ -4,7 +4,7 @@ var ChatApp = window.ChatApp || {};
 
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-    var currentUsername = 'Student';
+    var token = null;
 
     var lastChat = null;
 
@@ -53,101 +53,112 @@ var ChatApp = window.ChatApp || {};
     };
 
     ChatApp.populateChats = function () {
-        apiClient.conversationsGet({}, null, {})
-            .then(function (result) {
+        ChatApp.useToken(function (token) {
+            apiClient.conversationsGet({}, null, {headers: {Authorization: token}})
+                .then(function (result) {
+                    var currentUsername = userPool.getCurrentUser().getUsername();
 
-                result.data.forEach(function (convo) {
-                    var otherUsers = [];
-                    convo.participants.forEach(function (user) {
-                        if (user !== currentUsername) {
-                            otherUsers.push(user);
+                    result.data.forEach(function (convo) {
+                        var otherUsers = [];
+                        convo.participants.forEach(function (user) {
+                            if (user !== currentUsername) {
+                                otherUsers.push(user);
+                            }
+                        });
+
+                        var last = '&nbsp;';
+                        if (convo.last) {
+                            last = moment(new Date(convo.last)).fromNow();
                         }
+
+                        $('TBODY').append('<tr><td><a href="chat.html#' + convo.id + '">' + otherUsers.join(', ') + '</a></td><td>' + last + '</td></tr>');
                     });
-
-                    var last = '&nbsp;';
-                    if (convo.last) {
-                        last = moment(new Date(convo.last)).fromNow();
-                    }
-
-                    $('TBODY').append('<tr><td><a href="chat.html#' + convo.id + '">' + otherUsers.join(', ') + '</a></td><td>' + last + '</td></tr>');
+                    $('TBODY').append('<tr><td></td><td></td></tr>');
                 });
-                $('TBODY').append('<tr><td></td><td></td></tr>');
-            });
+        });
     };
 
     ChatApp.loadChat = function () {
-        apiClient.conversationsIdGet({id: location.hash.substring(1)}, null, {})
-            .then(function (result) {
-                var lastRendered = lastChat === null ? 0 : lastChat;
-                if((lastChat === null && result.data.last) || lastChat < result.data.last) {
-                    lastChat = result.data.last;
-                } else {
-                    return;
-                }
-                result.data.messages.forEach(function (message) {
-                    if(message.time > lastRendered) {
-                        var panel = $('<div class="panel">');
-                        if (message.sender === currentUsername) {
-                            panel.addClass('panel-default');
-                        } else {
-                            panel.addClass('panel-info');
-                            panel.append('<div class="panel-heading">' + message.sender + '</div>');
-                        }
-                        var body = $('<div class="panel-body">').text(message.message);
-                        panel.append(body);
-                        panel.append('<div class="panel-footer messageTime" data-time="' + message.time + '">' + moment(message.time).fromNow() + '</div>');
-
-                        var row = $('<div class="row">');
-                        var buffer = $('<div class="col-xs-4">');
-                        var holder = $('<div class="col-xs-8">');
-                        holder.append(panel);
-
-                        if (message.sender === currentUsername) {
-                            row.append(buffer);
-                            row.append(holder);
-                        } else {
-                            row.append(holder);
-                            row.append(buffer);
-                        }
-
-                        $('#chat').append(row);
+        var currentUsername = userPool.getCurrentUser().getUsername();
+        ChatApp.useToken(function (token) {
+            apiClient.conversationsIdGet({id: location.hash.substring(1)}, null, {headers: {Authorization: token}})
+                .then(function (result) {
+                    var lastRendered = lastChat === null ? 0 : lastChat;
+                    if((lastChat === null && result.data.last) || lastChat < result.data.last) {
+                        lastChat = result.data.last;
+                    } else {
+                        return;
                     }
+                    result.data.messages.forEach(function (message) {
+                        if(message.time > lastRendered) {
+                            var panel = $('<div class="panel">');
+                            if (message.sender === currentUsername) {
+                                panel.addClass('panel-default');
+                            } else {
+                                panel.addClass('panel-info');
+                                panel.append('<div class="panel-heading">' + message.sender + '</div>');
+                            }
+                            var body = $('<div class="panel-body">').text(message.message);
+                            panel.append(body);
+                            panel.append('<div class="panel-footer messageTime" data-time="' + message.time + '">' + moment(message.time).fromNow() + '</div>');
+
+                            var row = $('<div class="row">');
+                            var buffer = $('<div class="col-xs-4">');
+                            var holder = $('<div class="col-xs-8">');
+                            holder.append(panel);
+
+                            if (message.sender === currentUsername) {
+                                row.append(buffer);
+                                row.append(holder);
+                            } else {
+                                row.append(holder);
+                                row.append(buffer);
+                            }
+
+                            $('#chat').append(row);
+                        }
+                    });
+                    window.scrollTo(0, document.body.scrollHeight);
                 });
-                window.scrollTo(0, document.body.scrollHeight);
-            });
+        });
     };
 
     ChatApp.send = function () {
-        apiClient.conversationsIdPost({id: location.hash.substring(1)}, $('#message').val(), {})
-            .then(function () {
-                $('#message').val('').focus();
-                ChatApp.loadChat();
-            });
-
+        // We can assume the token will be set by now
+        ChatApp.useToken(function(token) {
+            apiClient.conversationsIdPost({id: location.hash.substring(1)}, $('#message').val(), {headers: {Authorization: token}})
+                .then(function () {
+                    $('#message').val('').focus();
+                    ChatApp.loadChat();
+                });
+        });
     };
 
     ChatApp.populatePeople = function () {
-        apiClient.usersGet({}, null, {})
-            .then(function (result) {
-                result.data.forEach(function (name) {
-                    var button = $('<button class="btn btn-primary">Start Chat</button>');
-                    button.on('click', function() {
-                        ChatApp.startChat(name);
-                    });
+        ChatApp.useToken(function (token) {
+            apiClient.usersGet({}, null, {headers: {Authorization: token}})
+                .then(function (result) {
+                    result.data.forEach(function (name) {
+                        var button = $('<button class="btn btn-primary">Start Chat</button>');
+                        button.on('click', function() {
+                            ChatApp.startChat(name);
+                        });
 
-                    var row = $('<tr>');
-                    row.append('<td>' + name + '</td>');
-                    var cell = $('<td>');
-                    cell.append(button);
-                    row.append(cell);
-                    $('TBODY').append(row);
+                        var row = $('<tr>');
+                        row.append('<td>' + name + '</td>');
+                        var cell = $('<td>');
+                        cell.append(button);
+                        row.append(cell);
+                        $('TBODY').append(row);
+                    });
+                    $('TBODY').append('<tr><td></td><td></td></tr>');
                 });
-                $('TBODY').append('<tr><td></td><td></td></tr>');
-            });
+        });
     };
 
     ChatApp.startChat = function (name) {
-        apiClient.conversationsPost({}, [name], {})
+        // We know the token will be set by now
+        apiClient.conversationsPost({}, [name], {headers: {Authorization: token}})
             .then(function (result) {
                 window.location = '/chat.html#' + result.data;
             });
@@ -196,6 +207,23 @@ var ChatApp = window.ChatApp || {};
                 alert(err);
             }
         })
+    };
+
+    ChatApp.useToken = function (callback) {
+        if (token === null) {
+            var cognitoUser = userPool.getCurrentUser();
+            if (cognitoUser !== null) {
+                cognitoUser.getSession(function (err, session) {
+                    if (err) {
+                        window.location = '/';
+                    }
+                    token = session.getIdToken().getJwtToken();
+                    callback(token);
+                });
+            }
+        } else {
+            callback(token);
+        }
     };
 
 }(jQuery));
